8e88d74db915b40c60d7db6948c797b6f660f65b
appiah0 <111425714+appiah0@users.noreply.github.com>
2026-05-25 15:34:35 +0000

    Add files via upload

--- app/entry/ [id]/edit.tsx
+++ app/entry/ [id]/edit.tsx
@@ -1,1 +1,8 @@
+// app/entry/[id]/edit.tsx
+import { useLocalSearchParams } from 'expo-router';
+import EntryForm from '../../../src/components/EntryForm';
 
+export default function EditEntry() {
+  const { id } = useLocalSearchParams<{ id: string }>();
+  return <EntryForm mode="edit" entryId={id} />;
+}
