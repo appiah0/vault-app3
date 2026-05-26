41db4c1326286176730e9301d0a82764edb03411
appiah0 <111425714+appiah0@users.noreply.github.com>
2026-05-26 09:29:49 +0000

    Delete app/entry/new.tsx

--- app/entry/new.tsx
+++
@@ -1,9 +0,0 @@
-// app/entry/new.tsx and app/entry/[id]/edit.tsx share this component
-// app/entry/new.tsx
-import React, { useState } from 'react';
-import { useLocalSearchParams } from 'expo-router';
-import EntryForm from '../../src/components/EntryForm';
-
-export default function NewEntry() {
-  return <EntryForm mode="new" />;
-}
